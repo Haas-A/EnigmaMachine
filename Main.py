8719fcac4103b344900cc420ff561db02c6336e2
Alex <martinhaas6@gmail.com>
2022-03-13 10:09:15 -0400

    Plugboard Functional

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
 #Main Methods of the Enigma Machine
+from Plugboard import Plugboard
+# import Scrambler
+
+switchLetters = ["AB", "CD"]
+board = Plugboard(switchLetters)
+
+line = "ABCD"
+newLine = board.switch(line)
+print(newLine)
--- Plugboard.py
+++ Plugboard.py
@@ -4,24 +4,27 @@
 #lettersToSwitch will contain the letters to switch in the format ("AB", "EY", etc.)
 #message will be a list of strings, each string being a line in the message
 
-class Plugboard(lettersToSwitch):
+class Plugboard():
     plugSwitches = {}
-    def __init__(self, lettersToSwitch, Lines):
+    lettersToSwitch = []
+    def __init__(self, lettersToSwitch):
+        plugSwitch = {}
         for pair in lettersToSwitch:
             letOne = pair[0]
             letTwo = pair[1]
-            plugSwitches[letOne] = letTwo
-            plugSwitches[letTwo] = letOne
+            self.plugSwitches[letOne] = letTwo
+            self.plugSwitches[letTwo] = letOne
+        plugSwitches = plugSwitch
     def switch(self, message):
-        switchedMessage = []
+        switchedMessage = ""
         for line in message:
             switchedLine = ""
             for letter in line:
-                if letter in plugSwitches:
-                    switchedLine = switchedLine + plugSwitches[letter]
+                if letter in self.plugSwitches:
+                    switchedLine = switchedLine + self.plugSwitches[letter]
                 else:
                     switchedLine = switchedLine + letter
-            switchedMessage = switchedMessage + switchedLine + "\n"
+            switchedMessage = switchedMessage + switchedLine
         return switchedMessage
     #class here
 #yo
